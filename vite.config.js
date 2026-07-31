70a8cf4f62001fa3b90968cef362577eed6b3cab
andrewmykolyshyn-cmyk <andrewmykolyshyn@gmail.com>
2026-07-31 18:51:16 +0200

    Add path alias for src directory in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     react(),
     legacy({
