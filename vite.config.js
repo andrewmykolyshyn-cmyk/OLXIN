412faef41a678033d3b6d546c826e3acd51b9c6b
andrewmykolyshyn-cmyk <andrewmykolyshyn@gmail.com>
2026-07-31 16:51:40 +0200

    Add path aliasing in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 5173,
     proxy: {
@@ -14,7 +20,6 @@ export default defineConfig({
     },
   },
   build: {
-    base: './',
     outDir: 'dist',
     sourcemap: true,
   },
