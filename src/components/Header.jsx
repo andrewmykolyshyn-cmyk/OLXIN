1ada4abe5cb31fdf694711e41de9c87ef610d685
andrewmykolyshyn-cmyk <andrewmykolyshyn@gmail.com>
2026-08-01 19:49:06 +0200

    Fix indentation in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,7 +47,6 @@ export default function Header({ favoritesCount }) {
   const handleNav = (path) => {
   navigate(path);
 };
-  };
 
   const handleLangChange = (e) => {
     setLang(e.target.value);
